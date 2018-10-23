dc6df3442240b5ca0143b55362a4b5b745e0c0f0
jakubfilistynski <36443903+JAck0b@users.noreply.github.com>
2018-10-23 20:50:53 +0200

    Update App.java

--- src/main/java/ebookjakubfilistynski/App.java
+++ src/main/java/ebookjakubfilistynski/App.java
@@ -8,7 +8,7 @@ public class App {
  * @param args Required parameter.
  */
   public static void main(String[] args) {
-    System.out.println("2");
+    System.out.println("3");
     Scanner in = new Scanner(System.in);
     String command = "";
     System.out.println("");
